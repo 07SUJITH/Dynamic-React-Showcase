ad16159d956d3f3e5f3e2ca2ed8e42fbb681ba34
07SUJITH <tssujith2002@gmail.com>
2024-06-30 20:23:52 +0530

    feat: Add fadeIn animation to home page

---
+++ src/pages/home/animation.js
@@ -0,0 +1,9 @@
+export const fadeIn = {
+    initial: {
+        opacity: 0,
+    },
+    enter: {
+        opacity: 1,
+        transition: { duration: 0.6, ease: [0.33, 1, 0.68, 1], delay: 0.5 },
+    },
+}
