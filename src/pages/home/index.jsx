a16627c586b1acd83c895f435797254707590848
07SUJITH <tssujith2002@gmail.com>
2024-06-30 20:24:16 +0530

    feat: Add home page component and styles

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            {' '}
+            Home page
+        </div>
+    )
+}
+
+export default Home
